3c8e3ffc5b46501c7fe487c7d553cbf2b2023726
yilmazsemi <156808751+yilmazsemi@users.noreply.github.com>
2024-05-23 02:26:11 +0300

    semuel
    change

--- Test_03.05/Assets/Ball.cs
+++ Test_03.05/Assets/Ball.cs
@@ -6,27 +6,24 @@ using Photon.Pun;
 public class Ball : MonoBehaviourPun
 {
     Rigidbody rb;
-    Photon.Realtime.Player ownerPlayer; // Topun sahibini tutmak için
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        ownerPlayer = photonView.Owner; // Topun sahibini ayarla
-        if (PhotonNetwork.IsMasterClient) // Eğer mevcut oyuncu ev sahibi ise
-        {
-            photonView.TransferOwnership(ownerPlayer); // Topun sahibini belirle
-        }
     }
 
     void Update()
     {
-        if (PhotonNetwork.IsConnected && ownerPlayer != null && PhotonNetwork.LocalPlayer != ownerPlayer)
+        // Photon ağına bağlı mı kontrolü yapılır
+        if (PhotonNetwork.IsConnected)
         {
-            rb.isKinematic = true; // Sadece topun sahibi tarafından kontrol edilmesini sağla
+            // Photon ağı bağlantısı mevcut ise her oyuncunun topu kontrol etmesine izin verilir
+            rb.isKinematic = false;
         }
         else
         {
-            rb.isKinematic = false; // Diğer oyuncuların topu kontrol etmesine izin ver
+            // Photon ağına bağlı değilse, yani oyuncular tek başına oyunu oynuyorsa, yalnızca yerel oyuncunun topu kontrol etmesine izin verilir
+            rb.isKinematic = !photonView.IsMine;
         }
     }
 }
