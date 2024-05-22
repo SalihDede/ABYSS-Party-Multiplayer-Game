4d693e1280b427b99c975d628a630ba047ecfc1c
Mo Binni <mo@minni.io>
2024-05-22 16:46:39 +0300

    Update Ball.cs

--- Test_03.05/Assets/Ball.cs
+++ Test_03.05/Assets/Ball.cs
@@ -28,36 +28,6 @@ public class Ball : MonoBehaviour
         yield return new WaitForSeconds(3);
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
 
-        if(other.tag == "0" || other.tag == "1" || other.tag == "2" || other.tag == "3")
-        {
-            if(other.tag == "0")
-            {
-                SemihGame.GetComponent<GameOneManager>().Player1 += 1;
-            }
-            if (other.tag == "1")
-            {
-                SemihGame.GetComponent<GameOneManager>().Player2 += 1;
-            }
-            if (other.tag == "2")
-            {
-                SemihGame.GetComponent<GameOneManager>().Player3 += 1;
-            }
-            if (other.tag == "3")
-            {
-                SemihGame.GetComponent<GameOneManager>().Player4 += 1;
-            }
-            SemihGame.GetComponent<GameOneManager>().LastTouch.text = other.tag;
-        }
-
-        if(other.tag == "NET1")
-        {
-            StartCoroutine(Effect());
-            Destroy(gameObject);
-            SemihGame.GetComponent<GameOneManager>().Goal = true;
-        }
-    }
 
 }
