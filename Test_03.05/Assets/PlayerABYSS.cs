7c514706a6cdfbea34788b004f01204b8f21e35a
yilmazsemi <156808751+yilmazsemi@users.noreply.github.com>
2024-05-23 03:36:19 +0300

    change

--- Test_03.05/Assets/PlayerABYSS.cs
+++ Test_03.05/Assets/PlayerABYSS.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class PlayerABYSS : MonoBehaviour
 {
-
     public Animator Anim;
+
+    public float shotForce = 500f; // Şut kuvveti
+    private bool canShoot = true; // Tekrar tekrar şut yapmayı engellemek için kullanılacak flag
 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        // Koşma animasyonunu kontrol et
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
         {
-            Anim.SetBool("IsRun",true);
+            Anim.SetBool("IsRun", true);
         }
         else
         {
             Anim.SetBool("IsRun", false);
         }
-        if(Input.GetKeyDown(KeyCode.Space))
+
+        // Zıplama animasyonunu kontrol et
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Anim.SetBool("IsJump", true);
         }
@@ -31,5 +35,37 @@ public class PlayerABYSS : MonoBehaviour
         {
             Anim.SetBool("IsJump", false);
         }
+
+        // Topa vurma işlevselliği
+        if (Input.GetKeyDown(KeyCode.F) && canShoot)
+        {
+            ShootBall();
+        }
+    }
+
+    // Topa vurma fonksiyonu
+    private void ShootBall()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider.CompareTag("Ball"))
+            {
+                Rigidbody ballRb = hit.collider.GetComponent<Rigidbody>();
+                if (ballRb != null)
+                {
+                    ballRb.AddForce(transform.forward * shotForce);
+                    canShoot = false; // Tekrar tekrar şut yapmayı engelle
+                    StartCoroutine(ResetShootFlag());
+                }
+            }
+        }
+    }
+
+    // Tekrar tekrar şut yapmayı engelleme için flag resetleme fonksiyonu
+    IEnumerator ResetShootFlag()
+    {
+        yield return new WaitForSeconds(1f); // 1 saniye sonra flag'i tekrar true yap
+        canShoot = true;
     }
 }
