a27374662bb08c36807e606d5acf3cc0b2fafdcc
Mo Binni <mo@minni.io>
2024-05-23 03:39:37 +0300

    asd

--- Test_03.05/Assets/PlayerABYSS.cs
+++ Test_03.05/Assets/PlayerABYSS.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class PlayerABYSS : MonoBehaviour
 {
-    public Animator Anim;
 
-    public float shotForce = 500f; // Şut kuvveti
-    private bool canShoot = true; // Tekrar tekrar şut yapmayı engellemek için kullanılacak flag
+    public Animator Anim;
 
     void Start()
     {
-        
+
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // Koşma animasyonunu kontrol et
+
+
+
+
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
         {
             Anim.SetBool("IsRun", true);
@@ -25,8 +27,6 @@ public class PlayerABYSS : MonoBehaviour
         {
             Anim.SetBool("IsRun", false);
         }
-
-        // Zıplama animasyonunu kontrol et
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Anim.SetBool("IsJump", true);
@@ -35,37 +35,18 @@ public class PlayerABYSS : MonoBehaviour
         {
             Anim.SetBool("IsJump", false);
         }
-
-        // Topa vurma işlevselliği
-        if (Input.GetKeyDown(KeyCode.F) && canShoot)
-        {
-            ShootBall();
-        }
     }
 
-    // Topa vurma fonksiyonu
-    private void ShootBall()
+    private void OnTriggerStay(Collider other)
     {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        if (other.tag == "Ball")
         {
-            if (hit.collider.CompareTag("Ball"))
+            if (Input.GetMouseButtonDown(0))
             {
-                Rigidbody ballRb = hit.collider.GetComponent<Rigidbody>();
-                if (ballRb != null)
-                {
-                    ballRb.AddForce(transform.forward * shotForce);
-                    canShoot = false; // Tekrar tekrar şut yapmayı engelle
-                    StartCoroutine(ResetShootFlag());
-                }
+                Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+                rb.AddForce(transform.forward * 10f, ForceMode.Impulse);
             }
         }
     }
 
-    // Tekrar tekrar şut yapmayı engelleme için flag resetleme fonksiyonu
-    IEnumerator ResetShootFlag()
-    {
-        yield return new WaitForSeconds(1f); // 1 saniye sonra flag'i tekrar true yap
-        canShoot = true;
-    }
 }
