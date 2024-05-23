6753bc5706daf5704773364c5a889c0d9b3a04f1
Mo Binni <mo@minni.io>
2024-05-23 04:34:39 +0300

    Update DiceController.cs

--- Test_03.05/Assets/DiceController.cs
+++ Test_03.05/Assets/DiceController.cs
@@ -230,7 +230,7 @@ public class DiceController : MonoBehaviourPunCallbacks
         GameManager.GetComponent<GameManager>().MinigameCount = result;
         GameManager.GetComponent<GameManager>().MinigameList[GameManager.GetComponent<GameManager>().MinigameCount].SetActive(true);
         GameManager.GetComponent<GameManager>().Kamera.SetActive(false);
-        GameManager.GetComponent<GameManager>().GameGUI.SetActive(false);
+        GameManager.GetComponent<GameManager>().GUI.SetActive(false);
 
     }
 
