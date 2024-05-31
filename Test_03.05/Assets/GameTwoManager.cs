23809f1f42942d49c952297cef9e0c1abb9be474
Mo Binni <mo@minni.io>
2024-05-31 15:27:47 +0300

    Update GameTwoManager.cs

--- Test_03.05/Assets/GameTwoManager.cs
+++ Test_03.05/Assets/GameTwoManager.cs
@@ -104,6 +104,14 @@ public class GameTwoManager : MonoBehaviourPunCallbacks
             }
             Ranking.Clear();
 
+            foreach(GameObject player in GameManagerrr.GetComponent<GameManager>().PlayersSorted)
+            {
+                if(player.GetComponent<PhotonView>().IsMine)
+                {
+                    player.GetComponent<DiceController>().GUI.SetActive(true);
+                }
+            }
+
         }
     }
 
