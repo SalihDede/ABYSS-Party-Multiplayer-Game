4db0281f9784cce030bd3847e133419777ef3380
Mo Binni <mo@minni.io>
2024-05-31 11:55:40 +0300

    Update GameTwoManager.cs

--- Test_03.05/Assets/GameTwoManager.cs
+++ Test_03.05/Assets/GameTwoManager.cs
@@ -83,7 +83,7 @@ public class GameTwoManager : MonoBehaviourPunCallbacks
             //GameManagerrr.GetComponent<GameManager>().PlayersSorted.Sort((player1, player2) => player2.GetComponent<>().score.CompareTo(player1.GetComponent<GameFourPlayer>().score));
 
 
-
+            Ranking.Clear();
             foreach (GameObject player in Ranking)
             {
                 Destroy(player);
