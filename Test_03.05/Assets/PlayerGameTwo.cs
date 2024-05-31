86d75977ea9bd99524d25de31332c4e25dc6e311
Mo Binni <mo@minni.io>
2024-05-31 10:07:33 +0300

    Update PlayerGameTwo.cs

--- Test_03.05/Assets/PlayerGameTwo.cs
+++ Test_03.05/Assets/PlayerGameTwo.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class PlayerGameTwo : MonoBehaviour
+public class PlayerGameTwo : MonoBehaviourPunCallbacks
 {
 
     public GameObject GameManagerr;
