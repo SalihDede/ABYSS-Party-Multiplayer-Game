fd134ba6ec8d65811258a7893d10d93de5d69424
Mo Binni <mo@minni.io>
2024-06-02 01:36:26 +0300

    Update PlayerBehaviorForPuzzleGame.cs

--- Test_03.05/Assets/PlayerBehaviorForPuzzleGame.cs
+++ Test_03.05/Assets/PlayerBehaviorForPuzzleGame.cs
@@ -42,59 +42,9 @@ namespace KinematicCharacterController.Walkthrough.MultipleMovementStates
                 HandleTileSelection(); // Handle tile selection on mouse click
             }
 
-            HandleCharacterInput();
             HandlePuzzleInteraction(); // Handle puzzle interactions
         }
 
-        private void LateUpdate()
-        {
-            HandleCameraInput();
-        }
-
-        private void HandleCameraInput()
-        {
-            // Create the look input vector for the camera
-            float mouseLookAxisUp = Input.GetAxisRaw(MouseYInput);
-            float mouseLookAxisRight = Input.GetAxisRaw(MouseXInput);
-            Vector3 lookInputVector = new Vector3(mouseLookAxisRight, mouseLookAxisUp, 0f);
-
-            // Prevent moving the camera while the cursor isn't locked
-            if (Cursor.lockState != CursorLockMode.Locked)
-            {
-                lookInputVector = Vector3.zero;
-            }
-
-            // Input for zooming the camera (disabled in WebGL because it can cause problems)
-            float scrollInput = -Input.GetAxis(MouseScrollInput);
-#if UNITY_WEBGL
-            scrollInput = 0f;
-#endif
-
-            // Apply inputs to the camera
-            OrbitCamera.UpdateWithInput(Time.deltaTime, scrollInput, lookInputVector);
-
-            // Handle toggling zoom level
-            if (Input.GetMouseButtonDown(1))
-            {
-                OrbitCamera.TargetDistance = (OrbitCamera.TargetDistance == 0f) ? OrbitCamera.DefaultDistance : 0f;
-            }
-        }
-
-        private void HandleCharacterInput()
-        {
-            PlayerCharacterInputs characterInputs = new PlayerCharacterInputs();
-
-            // Build the CharacterInputs struct
-            characterInputs.MoveAxisForward = Input.GetAxisRaw(VerticalInput);
-            characterInputs.MoveAxisRight = Input.GetAxisRaw(HorizontalInput);
-            characterInputs.CameraRotation = OrbitCamera.Transform.rotation;
-            characterInputs.JumpDown = Input.GetKeyDown(KeyCode.Space);
-            characterInputs.CrouchDown = Input.GetKeyDown(KeyCode.C);
-            characterInputs.CrouchUp = Input.GetKeyUp(KeyCode.C);
-
-            // Apply inputs to character
-            Character.SetInputs(ref characterInputs);
-        }
 
         // Handle tile selection and movement
         private void HandleTileSelection()
