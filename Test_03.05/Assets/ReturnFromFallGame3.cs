f8518cce047b83f2fd0eeb17164bd262cddcbf69
Mo Binni <mo@minni.io>
2024-06-02 15:12:21 +0300

    asd

---
+++ Test_03.05/Assets/ReturnFromFallGame3.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnFromFallGame3 : MonoBehaviour
+{
+    public GameObject SpawnAgain;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Bullet")
+        {
+            other.gameObject.transform.position = SpawnAgain.transform.position;
+        }
+    }
+
+}
