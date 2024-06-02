aac0900f252afb27c5e4476e71ccd473a1f7beb8
Mo Binni <mo@minni.io>
2024-06-02 21:28:55 +0300

    Update GameOneManager.cs

--- Test_03.05/Assets/GameOneManager.cs
+++ Test_03.05/Assets/GameOneManager.cs
@@ -92,7 +92,7 @@ public class GameOneManager : MonoBehaviourPunCallbacks
 
                 GameManagerrr.GetComponent<GameManager>().PlayersSorted.Clear();
 
-                if (GameManagerrr.GetComponent<GameManager>().PlayersSorted.Count != 2)
+                if (GameManagerrr.GetComponent<GameManager>().PlayersSorted.Count != 4)
                 {
                     GameManagerrr.GetComponent<GameManager>().PlayersSorted.AddRange(GameManagerrr.GetComponent<GameManager>().PlayersTemp);
                 }
